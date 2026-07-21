040446c41eef7660aaae2ab0178c431fa04f9c4a
Glory Vijitha <153104637+Vijitha14@users.noreply.github.com>
2026-07-21 14:48:30 +0530

    Refactor MoodMate app to use new API client

--- moodmate_app.py
+++ moodmate_app.py
@@ -10,9 +10,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-genai.configure(api_key=GEMINI_API_KEY)
 
-model = genai.GenerativeModel("gemini-2.5-flash-pro")
+client = genai.Client(api_key=GEMINI_API_KEY)
 
 # ----------------------------
 # Streamlit UI
